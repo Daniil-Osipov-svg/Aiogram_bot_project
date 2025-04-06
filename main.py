38a659f5797076df954b8d74ecf1c84628f9dee2
Daniil <osipddddov048@mail.ru>
2025-04-06 15:47:12 +0900

    Начало работы с ботом

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-sentence = input("Enter a sentence: ").split(' ')
-pig_latin = []
-for i in sentence:
-    new_word = i[1:] + i[0]
-    pig_latin.append(new_word + 'ay')
-print(' '.join(pig_latin))
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+#API моего чат бота
+BOT_TOKEN = '7774687817:AAGlmbpI-HhrXadQsgu5LyWqNKlsOcEAx9E'
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+# Обработчик команды /start
+@dp.message(Command(commands=['start']))
+async def start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут TunWheel!')
+
+# Обработчик команды /help
+@dp.message(Command(commands=['help']))
+async def help_command(message: Message):
+    await message.answer('Я ваш помощник для подсчёта калорий.\n'
+                        'Я могу помочь вам с подсчётом калорий и составлением рациона питания.\n'
+                        'Напишите /start, чтобы начать!')
+
+#Эхо-бот
+@dp.message()
+async def echo_message(message: Message) -> None:
+    await message.reply(message.text or 'Пустое сообщение!')
+
+# Запуск бота
+if __name__ == '__main__':
+    dp.run_polling(bot)
