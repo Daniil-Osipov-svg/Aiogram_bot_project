1ccb5f4b4ffb3628f6a51276a925de6e55a7be4a
Daniil <osipddddov048@mail.ru>
2025-04-06 14:36:20 +0900

    Очередной тестовый коммит

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import aiogram
+sentence = input("Enter a sentence: ").split(' ')
+pig_latin = []
+for i in sentence:
+    new_word = i[1:] + i[0]
+    pig_latin.append(new_word + 'ay')
+print(' '.join(pig_latin))
